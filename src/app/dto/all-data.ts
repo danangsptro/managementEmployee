ac18c64734905e88e7fc030a0ee59424e555f726
danangsptro <danangsaputro300@gmail.com>
2024-03-11 18:40:16 +0700

    update dto

--- src/app/dto/all-data.ts
+++ src/app/dto/all-data.ts
@@ -1,15 +1,3 @@
-// export interface employeeData {
-//   username: string;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   birthDate: Date;
-//   BasicSalary: any;
-//   status: string;
-//   group: string;
-//   description: string;
-// }
-
 export class employeeData {
   username: string = '';
   firstName: string = '' ;
